ac6fb97d84fcf2c0e6e2fda487ab0b9dc32c09c2
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-06-19 14:10:07 +0530

    React

--- lec-9/src/App.js
+++ lec-9/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import { useState } from "react";
 
-function App() {
-  return (
+
+// function App() {
+//   const[name,setName]=useState('pushpa');
+//   function changeName(){ 
+//     console.log("you have click the function");
+//     setName("sankeerth");
+//   }
+//   return( 
+//     <div className="App">
+//       <div>Hello,{name}</div>
+//       <button onClick={changeName}>clickme</button>  
+
+//     </div>
+//   );
+// }
+// export default App;    
+
+
+import React from "react";
+import { useState } from "react"; 
+
+function App(){
+  const [name,setName]= useState('pushpa');
+  const [flag,setflag] = useState(false);
+  const [steps,setsteps] = useState(0);
+
+  function changeName(){
+    console.log("you have cliked the function");
+    setName ("king");  
+    setflag(!flag);
+  }
+  function increment(){
+    // setsteps (steps +1);  
+    // setsteps (steps +1); 
+
+    setsteps((prevstate) => prevstate +1);
+    setsteps((prevstate) => prevstate +1);
+
+  }
+  function decrement(){
+    return setsteps(steps -1);
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello,{flag? name:""}</div>
+      <button onClick={changeName}>clickme</button>
+      <hr></hr>
+      <button onClick={increment}>+</button>
+      <div>{steps}</div>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
 
 export default App;
+
+
+  
+
+
