3dca86bbd2881c9da915e7cb79c897fb1c212e16
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-06-24 13:03:25 +0530

    React

--- lec-10/src/App.js
+++ lec-10/src/App.js
@@ -1,60 +1,60 @@
-import { useState } from "react"; 
+import { useState } from "react";
 import "./App.css";
 
-function initialvalue(){
-  log ("function called");
+function initialvalue() {
+  console.log("function called");
   return 0;
 }
 
-function App(){
-  const[name,setName]= useState("king");
-  const[flag,setflag]=useState(false); 
-  const[steps,setsteps]=useState(0);  
-  // const[steps,setsteps]=useState(initialvalue);
-  const[names,setNames]=useState([]);  // Array //
+function App() {
+  const [name, setName] = useState("king");
+  const [flag, setflag] = useState(false);
+  // const[steps,setsteps]=useState(0);  
+  const [steps, setsteps] = useState(initialvalue());
+  const [names, setNames] = useState([]);  // Array //
 
 
-  function changeName(){ 
-    log("clicked");
-    return setflag("flag");
-  }   
+  function changeName() {
+    console.log("clicked");
+    return setflag(!flag);
+  }
 
-  function increment(){
-    setsteps ((prevstate) => prevstate + 1);
+  function increment() {
+    setsteps((prevstate) => prevstate + 1);
     setsteps((prevstate) => prevstate + 1);
   }
 
-  function decrement(){
-    return setsteps(steps_1);
+  function decrement() {
+    return setsteps(steps - 1);
   }
 
-  function addNames(e){
-    e.prevent0default ();
-    setNames([...names,{id:names.length.name}]);
+  function addNames(e) {
+    e.preventDefault();
+    setNames([...names, { id: names.length,name }]);
     setName("");
   }
-    return(
-      <div className="App">
-        <div> Hello,{flag? name:""}</div>
-        <button onclick = {changeName}>clicked</button>
-        <hr/>
-        <button onclick ={increment}>+</button>
-        <div>{seteps}</div>
-        <button onclick= {decrement}>-</button>
-        <hr/> 
+  return (
+    <div className="App">
+      <div> Hello,{flag ? name : ""}</div>
+      <button onclick={changeName}>clicked</button>
+      <hr />
+      <button onclick={increment}>+</button>
+      <div>{steps}</div>
+      <button onclick={decrement}>-</button>
+      <hr />
 
-        <form onSubmit={addNames}>
-        <input type="text" placeholder="add names" onChange={(e)=> setName(e.target.value)}></input> 
+      <form onSubmit={addNames}>
+        <input type="text" placeholder="add names" onChange={(e) => setName(e.target.value)}></input>
         <button>Submit</button>
-        </form>
-        <hr></hr> 
-        <ul> 
-          {names.map((item) =>{
-            <li key={item.id}>{item.name}</li>   // Todo list //
-          })}
-          </ul>       
-          </div>
-        );
-      }; 
+      </form>
+      <hr></hr>
+      <ul>
+        {names.map((item) => (
+          <li key={item.id}>{item.name}</li>   // Todo list //
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-        export default App;
+export default App;
