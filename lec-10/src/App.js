e029712694c30b806ac2497e10e3eb906d4cd90d
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-06-29 12:27:39 +0530

    React

--- lec-10/src/App.js
+++ lec-10/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"; 
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function initialvalue(){
+  log ("function called");
+  return 0;
 }
 
-export default App;
+function App(){
+  const[name,setName]= useState("king");
+  const[flag,setflag]=useState(false); 
+  const[steps,setsteps]=useState(0);  
+  // const[steps,setsteps]=useState(initialvalue);
+  const[names,setNames]=useState([]);  // Array //
+
+
+  function changeName(){ 
+    log("clicked");
+    return setflag("flag");
+  }   
+
+  function increment(){
+    setsteps ((prevstate) => prevstate + 1);
+    setsteps((prevstate) => prevstate + 1);
+  }
+
+  function decrement(){
+    return setsteps(steps_1);
+  }
+
+  function addNames(e){
+    e.prevent0default ();
+    setNames([...names,{id:names.length.name}]);
+    setName("");
+  }
+    return(
+      <div className="App">
+        <div> Hello,{flag? name:""}</div>
+        <button onclick = {changeName}>clicked</button>
+        <hr/>
+        <button onclick ={increment}>+</button>
+        <div>{seteps}</div>
+        <button onclick= {decrement}>-</button>
+        <hr/> 
+
+        <form onSubmit={addNames}>
+        <input type="text" placeholder="add names" onChange={(e)=> setName(e.target.value)}></input> 
+        <button>Submit</button>
+        </form>
+        <hr></hr> 
+        <ul> 
+          {names.map((item) =>{
+            <li key={item.id}>{item.name}</li>   // Todo list //
+          })}
+          </ul>       
+          </div>
+        );
+      }; 
+
+        export default App;
