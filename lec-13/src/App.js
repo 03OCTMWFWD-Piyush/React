0a18f886052a700d04a8aff1803057be956d4b23
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-07-05 14:46:04 +0530

    React

--- lec-13/src/App.js
+++ lec-13/src/App.js
@@ -8,7 +8,7 @@ import List from './List';
 
 function App(){
   const[number,setNumber]=useState(1);      
-  const[dark,setdark]=useState(false);              
+  const[dark,setDark]=useState(false);              
 
   // const getItems=() =>{
   //   return [number,number+1,number+2];
@@ -29,14 +29,14 @@ function App(){
   },[number]);    
 
     const theme ={       
-      backgroundcolor:dark ? '#333': '#fff',   
+      backgroundColor:dark ? '#333': '#fff',   
       color : dark ? '#fff':'#333'
     } 
     return(
       <div className="App">
         <div style={theme}>         
           <input type='number' value={number} onChange={e =>setNumber(parseInt(e.target.value))}/>
-          <button onClick={()=>setdark(prevDark =>! prevDark)}>Theme change </button> 
+          <button onClick={()=>setDark(prevDark => !prevDark)}>Theme change </button> 
           <List getItems = {getItems}/>
         </div>  
       </div>
