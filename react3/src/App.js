803b5c31820557fc589164bbe5f837b84bcd7970
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-07-31 12:47:35 +0530

    react

---
+++ react3/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Header from './Header';
+import Section from './Section';
+
+// created three separate component for 'nav', 'header' and 'next section'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />  
+      <Section />
+    </>
+  );                      
+}
+
+export default App;       
